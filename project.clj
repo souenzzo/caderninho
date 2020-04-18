ad7885829990351a69b66600de5cbf98da0576df
Enzzo Cavallo <souenzzo@gmail.com>
2020-04-18 16:42:22 -0300

    +main

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject br.com.souenzzo/caderninho "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :main br.com.souenzzo.caderninho)
