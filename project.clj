7d2ba0ffdc126ef24bc5e52e0a7f0c85de2d5702
Enzzo Cavallo <souenzzo@gmail.com>
2020-04-18 16:53:03 -0300

    +pedestal

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject br.com.souenzzo/caderninho "0.1.0"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]]
   :min-lein-version "2.0.0"
   :main br.com.souenzzo.caderninho)
--- src/br/com/souenzzo/caderninho.clj
+++ src/br/com/souenzzo/caderninho.clj
@@ -1,5 +1,30 @@
-(ns br.com.souenzzo.caderninho)
+(ns br.com.souenzzo.caderninho
+  (:require [clojure.edn :as edn]
+            [io.pedestal.http :as http]
+            [clojure.pprint :as pprint]
+            [ring.util.mime-type :as mime]))
 
+
+(defonce state (atom nil))
 (defn -main
   [& opts]
-  (prn {:opts opts :env (System/getenv) :props (System/getProperties)}))
+  (let [port (edn/read-string (System/getenv "PORT"))
+        handler (fn [req]
+                  {:body    (binding [*print-namespace-maps* false]
+                              (with-out-str (pprint/pprint req)))
+                   :headers {"Content-Type" "text/plain"}
+                   :status  200})
+        service {::http/port   port
+                 ::http/type   :jetty
+                 ::http/join?  false
+                 ::http/routes #{["/*path" :any handler
+                                  :route-name ::index*]
+                                 ["/" :any handler
+                                  :route-name ::index]}}]
+    (swap! state (fn [st]
+                   (when st
+                     (http/stop st))
+                   (-> service
+                       http/default-interceptors
+                       http/create-server
+                       http/start)))))
